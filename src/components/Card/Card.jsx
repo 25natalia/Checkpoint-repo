5f290dd52732b181e397948b7d7436702ddf01ce
25natalia <25natalia09garcia@gmail.com>
2025-03-09 19:37:19 -0500

    component 1 creado

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,10 @@
+const Card = ({ title }) => {
+	return (
+		<div>
+			<h2>{title}</h2>
+			<h1>{title}</h1>
+		</div>
+	);
+};
+
+export default Card;
---
+++ src/data/Card.js
@@ -0,0 +1,10 @@
+export const data = [
+	{
+		id: 1,
+		title: 'data1',
+	},
+	{
+		id: 2,
+		title: 'data2',
+	},
+];
